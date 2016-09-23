419617cea9aa5cde89273908f67cf583c60730e7
Mark <pagetribemail@gmail.com>
2016-09-23 20:33:42 +1000

    added open shift dir so it would find log dir

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var app = express()
 var path = require('path')
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000
 var serverIp = process.env.OPENSHIFT_NODEJS_IP || 'localhost'
+let logDir = process.env.OPENSHIFT_LOG_DIR || __dirname + '/logs'
 
 app.get('/', function(req, res) {
-  res.sendFile(path.normalize(__dirname + '/logs/price-data.json'))
+  res.sendFile(path.normalize(logDir + '/price-data.json'))
 })
 
 app.listen(port, serverIp, function() {
